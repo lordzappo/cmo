26c45bd1a8f32bb96c1133f715f9a244d2cceed7
Chris Harris <ccharris@gmail.com>
2017-06-30 13:12:15 -0400

    dumbass tmpdir

--- cmo/gatk.py
+++ cmo/gatk.py
@@ -5,7 +5,7 @@ logger = util.get_logger()
 
 
 class Gatk:
-    def __init__(self,version="default", java_version="default", java_args="-Xmx48g -Xms256m -XX:-UseGCOverheadLimit", temp_dir=None, mutect=False):
+    def __init__(self,version="default", java_version="default", java_args="-Xmx48g -Xms256m -XX:-UseGCOverheadLimit", temp_dir="/scratch", mutect=False):
         try:
             if mutect:
                 self.gatk_jar=util.programs["mutect"][version]
@@ -33,11 +33,11 @@ class Gatk:
             if value != None:
                 if isinstance(value, list):
                     for val in value:
-                        cmd = cmd + ["--"+arg,  val]
+                        cmd = cmd + ["--"+arg,  "'" + val + "'"]
                 elif value == True:
                     cmd = cmd + ["--"+arg]
                 elif value != False:
-                    cmd = cmd + ["--"+arg, value]
+                    cmd = cmd + ["--"+arg, "'" + value + "'"]
         return " ".join(cmd)
     def gatk_cmd_help(self, command):
         cmd = [self.java_cmd, self.java_args, "-jar", self.gatk_jar, "-T", command, " --help"]
